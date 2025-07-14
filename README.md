6de4afc90bcf8a4b72894934962a22d52598fa64
odalric <odalric.maillard@inria.fr>
2025-07-14 10:54:20 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# experiments
+# Experiments
 Experimentations with Learning Agents run on Statistial Reinforcement Learning environments.
+
+
+# Dependencies
+
+    "six==1.16.0",
+    "gymnasium==1.1.1",
+    "numpy==1.24.3",
+    "networkx==2.8.8",
+    "matplotlib==3.7.2",
+    "scipy==1.15.2",
+    "pydot==1.4.2",
+    "statisticalRL-environments=1.2025.07a",
+    "statisticalRL-learners=1.2025.07a"
---
+++ template/generictemplate.py
@@ -0,0 +1,48 @@
+from src.fullExperiment import runLargeMulticoreExperiment
+
+#######################
+# Import registered environments
+import environments.lib as bW
+#######################
+
+
+
+#######################
+# Instantiate one environment
+env = bW.make('river-swim-6')
+
+
+# Import some learners
+import learners.Generic.Qlearning as ql
+import learners.MDPs_discrete.UCRL3 as ucrl3
+import learners.MDPs_discrete.IMED_RL as imedrl
+import learners.MDPs_discrete.Optimal.OptimalControl  as opt
+#######################
+# Instantiate a few learners to be compared:
+agents = []
+nS = env.observation_space.n
+nA = env.action_space.n
+delta=0.05
+#agents.append( [random.Random, {"env": env.env}])
+agents.append( [ql.Qlearning, {"nS":nS, "nA":nA}])
+agents.append( [ucrl3.UCRL3_lazy, {"nS":nS, "nA":nA, "delta":delta}])
+agents.append(([imedrl.IMEDRL, {"nbr_states":nS, "nbr_actions":nA}]))
+
+#############################
+# Compute oracle policy:
+oracle = opt.build_opti(env.name, env, env.observation_space.n, env.action_space.n)
+
+#######################
+# Run a full experiment
+#######################
+import os
+from src.utils import get_project_root_dir
+ROOT= get_project_root_dir()+"/main/templates/"+"generictemplate"+"_results/"
+os.makedirs(ROOT,exist_ok=True)
+
+runLargeMulticoreExperiment(env, agents, oracle, timeHorizon=5000, nbReplicates=64,root_folder=ROOT)
+
+#######################
+# Plotting Regret directly from dump files of past runs:
+#files = plR.search_dump_cumRegretfiles("RiverSwim-6-v0", root_folder=ROOT)
+#plR.plot_results_from_dump(files, 500)
